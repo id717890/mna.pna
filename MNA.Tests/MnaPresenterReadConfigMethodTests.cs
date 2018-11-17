81bb150cf06441f261334b5df8d8258eb0362061
YusupovZA <jusupovz@gmail.com>
2018-11-17 21:18:46 +0500

    try to write unit test

---
+++ MNA.Tests/MnaPresenterReadConfigMethodTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MNA.Interfaces;
+using Moq;
+
+namespace MNA.Tests
+{
+    [TestClass]
+    public class MnaPresenterReadConfigMethodTests
+    {
+        [TestMethod]
+        public void MnaViewModelNotNullAfterReadConfig()
+        {
+            var mnaViewModelMoq = new Mock<IMnaViewModel>();
+            var mnaVie = new Mock<IMnaView>();
+            var presenterMoq = new Mock<IMnaPresenter>(mnaVie.Object);
+            //presenterMoq.Verify(x => x.ReadConfig());
+            //presenterMoq.Setup(x => x.ReadConfig());
+            //presenter.ReadConfig();
+        }
+    }
+}
